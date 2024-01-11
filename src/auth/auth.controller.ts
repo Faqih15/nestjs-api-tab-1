36d7d533ec6df9de9a321eed46a7a07366ee5abe
Faqih15 <mujahidfaqihuddin@gmail.com>
2024-01-11 18:19:58 +0700

    commit 1

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */ /* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  
+  @Post('signup')
+  signup() {
+    return 'im signup'
+  }
+
+  @Post('signin')
+  signin() {
+    return 'im signup'
+  }
+  
+}
